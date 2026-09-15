ba043549280a63563d26c1973693c3131d93f81b
roziarozia <rozialak@gmail.com>
2026-09-15 22:55:30 +0200

    Update robots.txt to include disallow rules
    Added disallow rules for owner and teacher panels in robots.txt

--- app/robots.js
+++ app/robots.js
@@ -2,7 +2,12 @@ const SITE = process.env.NEXT_PUBLIC_SITE_URL || 'https://gry.easygo-english.pl'
 
 export default function robots() {
   return {
-    rules: { userAgent: '*', allow: '/' },
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      // Panel właścicielki – wyszukiwarki nie mają go indeksować ani pokazywać.
+      disallow: ['/panel-wlascicielki', '/panel-lektora'],
+    },
     sitemap: SITE + '/sitemap.xml',
   };
 }
